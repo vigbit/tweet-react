e91c76fdd2667c93fd1f075863f0f97cbd5400c2
vigbit <vigneshb@live.com>
2020-05-28 23:36:03 +0800

    "first retrieval"

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -3,18 +3,50 @@ import ReactDOM from 'react-dom';
 
 import tweets from 'tweets'
 
+
+
+// class ListItem extends React.Component {
+
+//     render() {
+
+//       console.log(this.props)
+
+//       let listColors = this.props.tweets.tweets.map(color => {
+//           return <li>{color}</li>
+//       })
+
+//         return (
+//           <div>
+//           <li>{this.props.tweets.tweet}</li>
+//           <li>{this.props.item.weight}</li>
+//           <li>{listColors}</li>
+//           </div>
+
+//         );
+//     }
+// }
+
+
 class App extends React.Component {
   render() {
+      console.log(this.props.tweets.tweets)
+      let userTweets = this.props.tweets.tweets.map( (tweet) => {
+        return <li>{tweet.user.screen_name}</li>
+      })
+
     return (
-      <div>
+      <div id="app">
         tweets
+        {userTweets}
       </div>
+
     );
   }
 }
 
 const element = document.getElementById('app');
 
-ReactDOM.render(<App />, element );//
+ReactDOM.render(<App tweets={tweets}/>, element );//
 
 console.log("tweet react");
+
